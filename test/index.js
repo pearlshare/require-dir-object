319243a280da0c8754fbdc78244e8a745775ff98
ExPHAT <me@exphat.com>
2015-09-04 15:43:38 -0400

    cleanup and test fixes hard-coded results into tests removed helper methods from exported function spelling description updates

--- test/index.js
+++ test/index.js
@@ -1,23 +1,6 @@
 var expect = require("expect.js");
 var path = require("path");
-var fs = require("fs");
 var reqDir = require("../");
-
-function jsAndDir(pth) {
-  // Returns an array of all JS and directories at a certain path
-
-  var found = fs.readdirSync(pth);
-  var js = [];
-
-  for (var i = 0; i < found.length; i++) {
-    var file = found[i].match(/[^\\]*\.(\w+)$/);
-    if ((file !== null && file[1] === "js") || fs.lstatSync(path.join(pth, found[i])).isDirectory()) {
-      js.push(found[i]);
-    }
-  }
-
-  return js;
-}
 
 describe("require-dir-object", function() {
   describe("configuration", function() {
@@ -52,29 +35,26 @@ describe("require-dir-object", function() {
     it("should return an empty object if the directory is empty", function() {
       var testDir = path.join(__dirname, "test-dir", "empty");
       var contents = reqDir(testDir);
-      var files = jsAndDir(testDir);
 
       expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(files.length);
+      expect(Object.keys(contents)).to.have.length(0);
     });
 
     it("should show the contents of a file within the directory", function() {
       var testDir = path.join(__dirname, "test-dir", "one-file");
       var contents = reqDir(testDir);
-      var files = jsAndDir(testDir);
 
       expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(files.length);
+      expect(Object.keys(contents)).to.have.length(1);
       expect(contents.one).to.eql(require(path.join(testDir, "one")));
     });
 
     it("should support multiple files in a single directory", function() {
       var testDir = path.join(__dirname, "test-dir", "two-files");
       var contents = reqDir(testDir);
-      var files = jsAndDir(testDir);
 
       expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(files.length);
+      expect(Object.keys(contents)).to.have.length(2);
       expect(contents.one).to.eql(require(path.join(testDir, "one")));
       expect(contents.two).to.eql(require(path.join(testDir, "two")));
     });
@@ -82,10 +62,9 @@ describe("require-dir-object", function() {
     it("should read a file even when there are directories along side it", function() {
       var testDir = path.join(__dirname, "test-dir");
       var contents = reqDir(testDir);
-      var files = jsAndDir(testDir);
 
       expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(files.length);
+      expect(Object.keys(contents)).to.have.length(4);
 
       expect(contents.empty).to.be.an("object");
       expect(contents["one-file"]).to.be.an("object");
