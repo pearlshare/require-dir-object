745d4c0c947b010e2ec5fde084bc08bef0e8ffb6
Oliver Brooks <oli@pearlshare.com>
2015-09-05 12:50:27 +0100

    add case conversion and index loading

--- test/index.js
+++ test/index.js
@@ -32,51 +32,179 @@ describe("require-dir-object", function() {
   });
 
   describe("usage", function() {
-    it("should return an empty object if the directory is empty", function() {
-      var testDir = path.join(__dirname, "test-dir", "empty");
-      var contents = reqDir(testDir);
 
-      expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(0);
+    describe("camelCase", function () {
+      it("should return an empty object if the directory is empty", function() {
+        var testDir = path.join(__dirname, "test-dir", "empty");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(0);
+      });
+
+      it("should show the contents of a file within the directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "one-file");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(1);
+        expect(contents.one).to.eql(require(path.join(testDir, "one")));
+      });
+
+      it("should support multiple files in a single directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "two-files");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(2);
+        expect(contents.one).to.eql(require(path.join(testDir, "one")));
+        expect(contents.two).to.eql(require(path.join(testDir, "two")));
+      });
+
+      it("should read a file even when there are directories along side it", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(5);
+
+        expect(contents.empty).to.be(undefined);
+        expect(contents["oneFile"]).to.be.an("object");
+        expect(contents["twoFiles"]).to.be.an("object");
+      });
+
+      it("should convert a file with underscores in the name to use camel case", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(contents.fileWithUnderscores).to.eql(require(path.join(testDir, "file_with_underscores")));
+      });
+
+      it("should use the index from a directory with an index file", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "camel"});
+
+        expect(contents).to.be.an("object");
+        expect(contents.withIndex).to.eql(require(path.join(testDir, "with_index")));
+      });
+
     });
 
-    it("should show the contents of a file within the directory", function() {
-      var testDir = path.join(__dirname, "test-dir", "one-file");
-      var contents = reqDir(testDir);
+    describe("snake_case", function () {
+      it("should return an empty object if the directory is empty", function() {
+        var testDir = path.join(__dirname, "test-dir", "empty");
+        var contents = reqDir(testDir, {case: "snake"});
 
-      expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(1);
-      expect(contents.one).to.eql(require(path.join(testDir, "one")));
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(0);
+      });
+
+      it("should show the contents of a file within the directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "one-file");
+        var contents = reqDir(testDir, {case: "snake"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(1);
+        expect(contents.one).to.eql(require(path.join(testDir, "one")));
+      });
+
+      it("should support multiple files in a single directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "two-files");
+        var contents = reqDir(testDir, {case: "snake"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(2);
+        expect(contents.one).to.eql(require(path.join(testDir, "one")));
+        expect(contents.two).to.eql(require(path.join(testDir, "two")));
+      });
+
+      it("should read a file even when there are directories along side it", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "snake"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(5);
+
+        expect(contents.empty).to.be(undefined);
+        expect(contents["one_file"]).to.be.an("object");
+        expect(contents["two_files"]).to.be.an("object");
+      });
+
+      it("should convert a file with underscores in the name to use snake case", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "snake"});
+
+        expect(contents).to.be.an("object");
+        expect(contents.file_with_underscores).to.eql(require(path.join(testDir, "file_with_underscores")));
+      });
+
+      it("should use the index from a directory with an index file", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "snake"});
+
+        expect(contents).to.be.an("object");
+        expect(contents.with_index).to.eql(require(path.join(testDir, "with_index")));
+      });
+
     });
 
-    it("should support multiple files in a single directory", function() {
-      var testDir = path.join(__dirname, "test-dir", "two-files");
-      var contents = reqDir(testDir);
+    describe("capitalize", function () {
+      it("should return an empty object if the directory is empty", function() {
+        var testDir = path.join(__dirname, "test-dir", "empty");
+        var contents = reqDir(testDir, {case: "capitalize"});
 
-      expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(2);
-      expect(contents.one).to.eql(require(path.join(testDir, "one")));
-      expect(contents.two).to.eql(require(path.join(testDir, "two")));
-    });
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(0);
+      });
 
-    it("should read a file even when there are directories along side it", function() {
-      var testDir = path.join(__dirname, "test-dir");
-      var contents = reqDir(testDir);
+      it("should show the contents of a file within the directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "one-file");
+        var contents = reqDir(testDir, {case: "capitalize"});
 
-      expect(contents).to.be.an("object");
-      expect(Object.keys(contents)).to.have.length(5);
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(1);
+        expect(contents["One"]).to.eql(require(path.join(testDir, "one")));
+      });
 
-      expect(contents.empty).to.be.an("object");
-      expect(contents["one-file"]).to.be.an("object");
-      expect(contents["two-files"]).to.be.an("object");
-    });
+      it("should support multiple files in a single directory", function() {
+        var testDir = path.join(__dirname, "test-dir", "two-files");
+        var contents = reqDir(testDir, {case: "capitalize"});
 
-    it("should convert a file with underscores in the name to use camel case", function() {
-      var testDir = path.join(__dirname, "test-dir");
-      var contents = reqDir(testDir);
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(2);
+        expect(contents["One"]).to.eql(require(path.join(testDir, "one")));
+        expect(contents["Two"]).to.eql(require(path.join(testDir, "two")));
+      });
 
-      expect(contents).to.be.an("object");
-      expect(contents.fileWithUnderscores).to.eql(require(path.join(testDir, "file_with_underscores")));
+      it("should read a file even when there are directories along side it", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "capitalize"});
+
+        expect(contents).to.be.an("object");
+        expect(Object.keys(contents)).to.have.length(5);
+
+        expect(contents.empty).to.be(undefined);
+        expect(contents["OneFile"]).to.be.an("object");
+        expect(contents["TwoFiles"]).to.be.an("object");
+      });
+
+      it("should convert a file with underscores in the name to use snake case", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "capitalize"});
+
+        expect(contents).to.be.an("object");
+        expect(contents["FileWithUnderscores"]).to.eql(require(path.join(testDir, "file_with_underscores")));
+      });
+
+      it("should use the index from a directory with an index file", function() {
+        var testDir = path.join(__dirname, "test-dir");
+        var contents = reqDir(testDir, {case: "capitalize"});
+
+        expect(contents).to.be.an("object");
+        expect(contents["WithIndex"]).to.eql(require(path.join(testDir, "with_index")));
+      });
+
     });
   });
 });
